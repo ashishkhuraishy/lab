c992763e5a86786be1d0f8ad9a046e24d39cbcc3
Ashish Khuraishy <ashishkhuraishy@gmail.com>
2020-11-09 17:30:53 +0000

    implemented sjf in c

--- main.c
+++ main.c
@@ -20,21 +20,33 @@ void sortProcess(int n){
 }
 
 
-void findWaitingTime(int n){
+void findTimeTaken(int n){
   process[0].waitingTime = 0;
   process[0].turnAroundTime =  process[0].waitingTime + process[0].burstTime;
 
   for(int i=1; i<n; i++){
-    process[0].turnAroundTime =  process[0].waitingTime + process[0].burstTime;
-    process[0].turnAroundTime =  process[0].waitingTime + process[0].burstTime;
+    process[i].waitingTime =  process[i - 1].waitingTime + process[i - 1].burstTime;
+    process[i].turnAroundTime =  process[i].waitingTime + process[i].burstTime;
   }
 
 }
 
-void findTimeTake(int n){
+void sjf(int n){
   int avgWT = 0, avgTAT = 0; 
 
   sortProcess(n);
+  findTimeTaken(n);
+
+  printf("\nProcess\tBurst Time\tWaiting Time\tTurn Around\n");
+
+  for(int i=0; i<n; i++){
+    printf("\nP%d\t\t%d\t\t\t%d\t\t\t\t%d\n", process[i].indx, process[i].burstTime, process[i].waitingTime, process[i].turnAroundTime);
+    avgWT = avgWT + process[i].waitingTime;
+    avgTAT = avgTAT + process[i].turnAroundTime;
+  }
+
+  printf("\nAvg Waiting Time -> %d\n", avgWT);
+  printf("Avg TurnAround Time -> %d\n", avgTAT);
 
 }
 
@@ -53,6 +65,7 @@ int main(void) {
     scanf("%d", &process[i].burstTime);
   }
 
+  sjf(n);
 
   return 0;
 }
